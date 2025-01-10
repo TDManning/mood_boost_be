6d067b6a5a62dea3f67d4e3b6c8af22436bb9df8
Danielle Cardona <danielle.cardona23@gmail.com>
2025-01-09 19:10:32 -0700

    update routes and user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,18 +1,34 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :username, :first_name, :password_digest, :email
+  attributes :username, :first_name, :email
 
   def self.format_user(user)
-    { data: 
-      {
-        id: user[:id]
+    { 
+      data: {
+        id: user.id.to_s,
         type: "user", 
         attributes: {
-          username: user[:attributes][:username],
-          first_name: user[:attributes][:first_name],
-          email: user[:attributes][:email]
+          username: user.username,
+          first_name: user.first_name,
+          email: user.email
         }
-      }}
+      }
+    }
   end
 
-
+  def self.format_users(users)
+    { 
+      data: users.map do |user|
+      {
+        id: user.id.to_s,
+        type: "user", 
+        attributes: {
+          username: user.username,
+          first_name: user.first_name,
+          email: user.email
+        }
+      }
+  end
+}
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :post, :patch]
+      resources :users, only: [:index, :create, :update]
     end
   end
 end
