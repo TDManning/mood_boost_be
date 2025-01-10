1653979ec8f06f701aa60368685d47c3b54fd875
Karl Fallenius <smilodon.p@gmail.com>
2025-01-10 14:13:59 -0700

    adds basic controller files and code

--- app/controllers/api/v1/users/activities_controller.rb
+++ app/controllers/api/v1/users/activities_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::Users::ActivitiesContoller < ApplicationController
-  
+  def index
+    user = User.find_by(id: params[:user_id])
+    if user
+      hosted_parties = ViewingParty.where(host_id: user.id).map do |party|
+  end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,20 @@
 class Api::V1:UsersController < ApplicationController
 
   def index
-
+    users= User.all
+    render json: UserSerializer.format_user_list(User.all)
   end
   
-  def post
-
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user), status: :created
+    else
+      render json: ErrorSerializer.format_error(ErrorMessage.new(user.errors.full_messages.to_sentence, 400)), status: :bad_request
+    end
   end
 
-  def patch
+  # def patch
 
-  end
+  # end
 end
