2824e83be48a552b23b6e73d8700b023964607ee
Danielle Cardona <danielle.cardona23@gmail.com>
2025-01-13 16:08:08 -0700

    delete redundant password digest validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:username) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
-    it { should validate_presence_of(:password_digest) }
     it { should validate_length_of(:first_name).is_at_most(20).allow_nil }
   end
 
