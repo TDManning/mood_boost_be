519961c59460ac3a834f14b0a9460f2a7e76972e
Terra Manning <terradmanning@gmail.com>
2025-01-12 18:19:19 -0700

    feat: create POST (create) endpoint for user activities in controller and model

--- app/controllers/api/v1/users/activities_controller.rb
+++ app/controllers/api/v1/users/activities_controller.rb
@@ -10,7 +10,13 @@ class Api::V1::Users::ActivitiesController < ApplicationController
   end
 
   def create
-
+    user_activity = UserActivity.create_new_activity(params[:user_id], params[:activity_id])
+  
+    if user_activity.persisted?
+      render json: { user_activity: user_activity }, status: :created
+    else
+      error_message = "Unable to create activity. Please ensure the user and activity are valid."
+      render json: { error: error_message }, status: :unprocessable_entity
+    end
   end
-
 end
--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -1,4 +1,20 @@
 class UserActivity < ApplicationRecord
   belongs_to :user
   belongs_to :activity
+
+  def self.get_user_activities(user_id)
+    user = User.find_by(id: user_id)
+    return nil if user.nil?
+    user.activities
+  end
+
+  def self.create_new_activity(user_id, activity_id)
+    user = User.find_by(id: user_id)
+    return UserActivity.new if user.nil?
+    create_with_user_and_activity(user.id, activity_id)
+  end
+
+  def self.create_with_user_and_activity(user_id, activity_id)
+    create(user_id: user_id, activity_id: activity_id)
+  end
 end
