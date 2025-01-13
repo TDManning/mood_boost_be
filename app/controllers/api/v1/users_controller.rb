07a59da85463af1cb5cd6e5d8e2c96c18793e49d
Danielle Cardona <danielle.cardona23@gmail.com>
2025-01-13 15:29:03 -0700

    fix: delete redundant password in model and update controller to include password confirmation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -32,6 +32,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :first_name, :email)
+    params.require(:user).permit(:username, :password, :password_confirmation, :first_name, :email)
   end
 end
