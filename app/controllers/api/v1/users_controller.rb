89ff725c61ed8df41362c949fc872c5dac985b63
Terra Manning <terradmanning@gmail.com>
2025-02-09 18:04:37 -0700

    feat: add functionality for a guest user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::UsersController < ApplicationController
-
   def index
-    users= User.all
-    render json: UserSerializer.format_users(User.all)
+    users = User.all
+    render json: UserSerializer.format_users(users)
   end
   
   def create
@@ -14,13 +13,36 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # def patch
-  #  STRETCH GOAL
-  # end
+  def guest_user
+    if session[:guest_user_id]
+      guest = User.find_by(id: session[:guest_user_id])
+      unless guest
+        session[:guest_user_id] = nil
+        return guest_user
+      end
+    else
+      guest = User.create!(
+        username: "guest_#{SecureRandom.hex(4)}",
+        email: "#{SecureRandom.hex(4)}@guest.com",
+        password: "GuestPass123!"
+      )
+      session[:guest_user_id] = guest.id
+    end
+  
+    render json: { guest_id: guest.id, username: guest.username }, status: :ok
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation, :first_name, :email)
+  end
+  
+  def create_guest_user
+    User.create!(
+      username: "guest_#{SecureRandom.hex(5)}",
+      email: "guest_#{SecureRandom.hex(5)}@example.com",
+      password: SecureRandom.hex(8)
+    )
   end
 end
